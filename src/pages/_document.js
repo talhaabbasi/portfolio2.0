d17bf61cc5c29d78f810231a3a8b5ff035e630fd
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-06-13 20:31:30 +0500

    Update head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Html, Main, NextScript } from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -29,15 +29,24 @@ export default class MyDocument extends Document {
   }
   render() {
     return (
-      <Html lang='en-GB'>
+      <Html lang="en-GB">
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <title>Talha Abbasi</title>
+          <meta
+            name="keywords"
+            content="web development, programming, portfolio, developer"
+          />
+          <link rel="icon" href="images/logo.png" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
