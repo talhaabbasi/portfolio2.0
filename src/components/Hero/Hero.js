7b7fe846b022da5bb7b2752316e7254bece8766d
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-06-13 14:05:25 +0500

    Hero Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react"
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents"
+import Button from "../../styles/GlobalComponents/Button"
+import { LeftSection } from "./HeroStyles"
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there! <br />I am Talha Abbasi
+      </SectionTitle>
+      <a
+        href="mailto:talhaabbasi1997@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button>Contact</Button>
+      </a>
+    </LeftSection>
+  </Section>
+)
 
-export default Hero;
+export default Hero
