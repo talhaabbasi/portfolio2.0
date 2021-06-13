008c3cb9c7073b30d91e75038e2b801c814fae60
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-06-13 23:56:45 +0500

    Add Technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,72 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react"
+import { DiDatabase, DiReact } from "react-icons/di"
+import { BsCode, BsCloudFill } from "react-icons/bs"
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents"
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles"
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
-);
+const Technologies = () => (
+  <Section>
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies in the web development world.
+      From Front-end to Back-end delivering complete issues.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-end</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React along with frameworks like Gatsby and Next.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <BsCode size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-end</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node.js, Python and ASP.Net
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiDatabase size="3rem" />
+        <ListContainer>
+          <ListTitle>Databases</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            MongoDb, DynamoDb and Microsoft SQL Server
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <BsCloudFill size="3rem" />
+        <ListContainer>
+          <ListTitle>Cloud Services</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Amazon Web Services
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
+)
 
-export default Technologies;
+export default Technologies
