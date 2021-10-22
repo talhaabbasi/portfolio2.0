4fb65dc5188384c14fed53f43ff4347a08cb12cd
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-10-22 21:38:59 +0500

    Add Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,21 @@
-import Theme from "../styles/theme"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import * as gtag from "../../lib/gtag"
 import Head from "next/head"
+import Theme from "../styles/theme"
 
-export default function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Theme>
@@ -13,3 +27,5 @@ export default function App({ Component, pageProps }) {
     </>
   )
 }
+
+export default App
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from "next/document"
 import { ServerStyleSheet } from "styled-components"
+import { GA_TRACKING_ID } from "../../lib/gtag"
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -31,6 +32,23 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en-GB">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <meta
             name="keywords"
             content="web development, programming, portfolio, developer"
