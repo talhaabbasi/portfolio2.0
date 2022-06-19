6afb2ae7a01d2d634ddc32c3e9b2a5f8ea49cb35
talhaabbasi <talhaabbasi1997@gmail.com>
2022-06-19 13:17:58 +0500

    [FEAT] Add total experience function :muscle:

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -1,4 +1,8 @@
 import moment from "moment"
-export const dateToString = (date) => {
-  return moment(date).format("MMMM Y")
-}
+
+export const dateToString = (date) => moment(date).format("MMMM Y")
+
+export const totalExperience = (startDate, endDate) =>
+  endDate.getMonth() -
+  startDate.getMonth() +
+  12 * (endDate.getFullYear() - startDate.getFullYear())
